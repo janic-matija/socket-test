dc8a15d1c4473a6ae2c4960b36ea24adc46cce87
janic-matija <janic.j.matija@gmail.com>
2022-10-10 14:22:14 +0200

    ip

--- client2.py
+++ client2.py
@@ -1,8 +1,25 @@
 import socket
 
+HEADERSIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.connect(("10.18.110.76", 9998))
-#10.18.110.49
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+s.connect(('127.0.1.1', 9998))
+
+while True:
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print("new msg len: ", msg[:HEADERSIZE])
+            msglen = int(msg[:HEADERSIZE])
+            new_msg = False
+        print(f"full message length: {msglen}")
+        full_msg += msg.decode("utf-8")
+
+        print(len(full_msg))
+        if len(full_msg) - HEADERSIZE == msglen:
+            print("full message received")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
--- server2.py
+++ server2.py
@@ -1,5 +1,7 @@
 import socket
 
+HEADERSIZE = 10
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((socket.gethostname(), 9998))
@@ -9,4 +11,8 @@ while True:
     # now our endpoint knows about the OTHER endpoint.
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established.")
-    clientsocket.send(bytes("Hey there!!!", "utf-8"))
+
+    msg = "Welcome to the server!"
+    msg = f"{len(msg):<{HEADERSIZE}}" + msg
+
+    clientsocket.send(bytes(msg, "utf-8"))
