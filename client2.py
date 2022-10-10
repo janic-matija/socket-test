b538804ab5ccb85ad96cb10ab49cc59cd044aee4
janic-matija <janic.j.matija@gmail.com>
2022-10-10 11:26:23 +0200

    client

--- client2.py
+++ client2.py
@@ -2,7 +2,7 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.connect((socket.gethostname(), 9998))
-
+s.connect(("10.18.110.76", 9998))
+#10.18.110.49
 msg = s.recv(1024)
 print(msg.decode("utf-8"))
