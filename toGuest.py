33c54030ee8e1c59532325a0c0b5c2dd315d4b3b
janic-matija <janic.j.matija@gmail.com>
2022-10-28 14:55:13 +0200

    ...

--- toGuest.py
+++ toGuest.py
@@ -11,8 +11,6 @@ BUFFER_SIZE = 10737418240
 HOST = "10.18.110.76"
 SERVER_HOST = "0.0.0.0"  # any
 PORT = 9990
-# socketserver.TCPServer.request_queue_size = 100
-# print(socketserver.TCPServer.request_queue_size)
 
 if sys.argv[0] == '/home/matija/paramiko/fromHost.py':  # prima fajl
     os.system("mkdir -p /home/matija/from_host \n ")
@@ -20,9 +18,6 @@ if sys.argv[0] == '/home/matija/paramiko/fromHost.py':  # prima fajl
     file_recv = "/home/matija/from_host/big1G"
 
     recv_file_socket = socket.socket()
-    # print(socket.TCP_MAXSEG)
-    # recv_file_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_MAXSEG, 400)
-    # print(socket.TCP_MAXSEG)
     recv_file_socket.connect((HOST, PORT))
 
     with open(file_recv, "wb") as fr:
@@ -37,7 +32,6 @@ if sys.argv[0] == '/home/matija/paramiko/fromHost.py':  # prima fajl
 else:  # salje fajl
 
     send_file_socket = socket.socket()
-    # send_file_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_MAXSEG, 400)
 
     send_file_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     send_file_socket.bind((SERVER_HOST, PORT))
