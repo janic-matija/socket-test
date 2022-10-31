939ce9227f2ea1e504e42db0265f725c9deba19a
janic-matija <janci.j.matija@gmail.com>
2022-10-31 14:30:08 +0100

    ...

--- toHost.py
+++ toHost.py
@@ -6,11 +6,11 @@ import time
 
 start_time = time.time()
 
-BUFFER_SIZE = 8192
+BUFFER_SIZE = 1073741824
 HOST = "10.18.110.49"
-PORT = 9999
+PORT = 9996
 SERVER_HOST = "0.0.0.0"  # any
-SERVER_PORT = 9999
+SERVER_PORT = 9996
 
 stdin, stdout, stderr = "", "", ""
 
@@ -18,9 +18,10 @@ print(sys.argv[0])
 if sys.argv[0] == "/home/matija/Projects/socket-test/socket-test/fromVM.py":  # prima fajl
     print("fromVM u if")
 
-    file_recv = "/home/matija/Projects/socket-test/socket-test/recv/big1G"
+    file_recv = "/home/matija/Projects/socket-test/socket-test/recv/big100MB2"
 
     recv_file_socket = socket.socket()
+    recv_file_socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
     print("kreiran socket")
     recv_file_socket.connect((HOST, PORT))
 
@@ -43,7 +44,7 @@ else:  # salje fajl
 
     send_file_socket.listen(5)
     print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
-
+    print(f"listening {time.time()-start_time}")
     with open("IP/pw", 'r') as passFile:
         pw = passFile.readline()
     ssh = paramiko.SSHClient()
@@ -60,18 +61,23 @@ else:  # salje fajl
     print("poslato")
     stdin, stdout, stderr = ssh.exec_command("python3 /home/matija/Projects/socket-test/socket-test/fromVM.py")
     del ftp_client, stdin, stdout, stderr
+    print(f"ssh finished {time.time()-start_time}")
     # print(stdout.readlines())
     # print("izvrseno")
 
     client_socket, address = send_file_socket.accept()
     print(f"[+] {address} is connected.")
-    filename = "data/big1"
+    filename = "data/big3"
+    brojac = 0
     with open(filename, "rb") as f:
         while True:
             bytes_read = f.read(BUFFER_SIZE)
             if not bytes_read:
                 break
             client_socket.sendall(bytes_read)
+            if brojac == 0:
+                print(time.time()-start_time)
+                brojac = 1
     client_socket.close()
     send_file_socket.close()
 
