a4e906e12a7655fdfa4d324c16954afa8a90a121
janic-matija <janic.j.matija@gmail.com>
2022-11-14 10:44:33 +0100

    clean

--- rsyncToGuest.py
+++ rsyncToGuest.py
@@ -2,5 +2,5 @@ import time
 import os
 
 start = time.time()
-os.system("rsync -a ~/Projects/socket-test/data root@192.168.122.216:/tempdir")
+os.system("rsync -a ~/Projects/socket-test/socket-test/data root@192.168.122.17:/tempdir")
 print(time.time() - start)
--- testSSH.py
+++ testSSH.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 
 import paramiko
@@ -16,15 +17,19 @@ def ssh_send(hn, p, u, pw):
     ssh.connect(hostname=hn, port=p, username=u,
                 password=pw, timeout=3)
 
+    this_dir = str(subprocess.check_output(['pwd']))[2:-3] + "/"
+    this_file = this_dir + os.path.basename(__file__)
+    print(this_file)
+
     ftp_client = ssh.open_sftp()
-    ftp_client.put('/home/matija/Projects/socket-test/dirToGuest.py', '/home/ubuntuvm/fromHost.py')
+    ftp_client.put(this_file, '/home/matija/testSSH.py')
     ftp_client.close()
-    stdin, stdout, stderr = ssh.exec_command("python3 /home/ubuntuvm/fromHost.py")
-    # print(stdout.readlines())
+    stdin, stdout, stderr = ssh.exec_command("python3 /home/matija/testSSH.py")
+    print(stdout.readlines())
     del ftp_client, stdin, stdout, stderr
 
 
-if sys.argv[0] == '/home/ubuntuvm/testSSH.py':  # server prima fajl
+if sys.argv[0] == '/home/matija/testSSH.py':  # server prima fajl
     print("svasta")
 else:
-    ssh_send('192.168.122.216', 22, 'root', "IP/pw")
+    ssh_send('192.168.122.17', 22, 'root', "IP/pw")
