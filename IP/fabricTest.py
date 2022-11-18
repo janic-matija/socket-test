ef131358623dafbbc839437e5fc4925f5151b25a
janic-matija <janic.j.matija@gmail.com>
2022-11-18 16:14:20 +0100

    clean

--- IP/fabricTest.py
+++
@@ -1,9 +0,0 @@
-import fabric
-
-with open("pw", 'r') as passFile:
-    password = passFile.readline()
-
-c = fabric.Connection("10.18.110.49", port=22, user="root", connect_kwargs={'password': password})
-
-c.run("mkdir -p fabric35")
-c.run("cd fabric35 \n echo 'fabricTest' >> ftFile")
